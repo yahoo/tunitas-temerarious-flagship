9aa01e08c7437fb8dce3a917c7346271d78c4f8d
Wendell Baker <wbaker@oath.com>
2018-10-16 19:02:06 -0700

    add TF_DISAGGREGATE_COMPILE, TF_DEPENDENCIES_COMPILE

--- am/build.mk
+++ am/build.mk
@@ -1,6 +1,6 @@
 # A GNU_make-included -*- Makefile -*- fragment.
 included_temerarious_flagship_am_build_mk_included := 1
-ifeq "" :$(included_temerarious_flagship_am_build_am)"
+ifeq "" "$(included_temerarious_flagship_am_build_am)"
 $(warning instead include $(temerarious_flagship_datadir)/am/build.am - the 'am' directory, the 'am' suffix)
 include $(temerarious_flagship_datadir)/am/build.am
 endif
