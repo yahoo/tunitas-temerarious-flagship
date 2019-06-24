dc91418b34c29e056bb7787e32d93eddb1d583ec
Wendell Baker <wbaker@verizonmedia.com>
2019-06-24 12:12:10 -0700

    grammar in the LICENSE badging

--- am/build.mk
+++ am/build.mk
@@ -1,5 +1,6 @@
 # This is a GNU -*- Makefile -*-
-# Copyright 2019, Oath Inc. Licensed under the terms of the Apache-2.0 license. See LICENSE file in https://github.com/yahoo/tunitas-basics/blob/master/LICENSE for terms.
+# Copyright 2019, Oath Inc. Licensed under the terms of the Apache-2.0 license.
+# See the LICENSE file in https://github.com/yahoo/tunitas-basics/blob/master/LICENSE for terms.
 #
 # moved to .../am/build.am (suffix matches directory)
 # deprecating .../am/build.mk (suffix DOES NOT match directory)
